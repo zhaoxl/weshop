95d4037a479c6680d1543ad792b26f861288a5b5
赵晓龙 <99866770@qq.com>
2016-02-24 19:21:23 +0800

    商品图片管理制作完成

--- app/controllers/admin/product_logos_controller.rb
+++ app/controllers/admin/product_logos_controller.rb
@@ -1,12 +1,52 @@
-class Admin::ProductLogosController < ApplicationController
+class Admin::ProductLogosController < Admin::BaseController
   before_action :find_product
+  before_action :find_data, except: [:index, :new, :create]
   
   def index
     @logos = @product.product_logos
   end
   
+  def new
+    @logo = @product.product_logos.build
+  end
+  
+  def create
+    logo = @product.product_logos.build(post_params)
+    logo.save!
+    
+    redirect_to admin_product_product_logos_path(@product), notice: '添加成功'
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    @data.update_attributes(post_params)
+    
+    redirect_to admin_product_product_logos_path(@product), notice: '编辑成功'
+  end
+  
+  def move_down
+    @data.move_lower
+    redirect_to :back
+  end
+  
+  def move_up
+    @data.move_higher
+    redirect_to :back
+  end
+  
   private
   def find_product
     @product = Product.find(params[:product_id])
+  end
+  
+  def find_data
+    @data = ProductLogo.find(params[:id])
+  end
+  
+  def post_params
+    params.require(:product_logo).permit!
   end
 end
--- app/models/product_logo.rb
+++ app/models/product_logo.rb
@@ -1,4 +1,6 @@
 class ProductLogo < ActiveRecord::Base
+  acts_as_list
+  
   belongs_to  :product
   
   mount_uploader :logo, ProductLogoUploader
