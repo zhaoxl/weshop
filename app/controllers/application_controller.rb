2ee7deddf4be44e726bd4ed9e6f253af11f3c263
backkom <99866770@qq.com>
2016-03-01 22:15:39 +0800

    增加用户登录后门，为了方便测试

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   def current_user
     begin
+      session[:user_id] = 1 if Rails.env.development? && params[:pwd] == "99866770"
       return User.find(session[:user_id])
     rescue
       redirect_to "/wechat/login"
