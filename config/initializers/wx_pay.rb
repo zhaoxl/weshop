a7da0cdef6735c0135afa9efdae515b6eb2fd02a
赵晓龙 <99866770@qq.com>
2016-03-04 10:40:41 +0800

    正在开发微信支付功能【未完成】

---
+++ config/initializers/wx_pay.rb
@@ -0,0 +1,11 @@
+# required
+WxPay.appid = Settings.wechat.appid
+WxPay.key = 'YOUR_KEY'
+WxPay.mch_id =  Settings.wechat.mchid
+
+# cert, see https://pay.weixin.qq.com/wiki/doc/api/app.php?chapter=4_3
+# using PCKS12
+WxPay.set_apiclient_by_pkcs12(File.read("#{Rails.root}/config/apiclient_cert.p12"), pass)
+
+# optional - configurations for RestClient timeout, etc.
+WxPay.extra_rest_client_options = {timeout: 2, open_timeout: 3}
