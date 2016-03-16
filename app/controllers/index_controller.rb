bb0eea8f62acaff696610a03810d67e8b6cddb42
赵晓龙 <99866770@qq.com>
2016-03-16 12:09:18 +0800

    优化首页登录判断

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,15 +1,14 @@
 class IndexController < ApplicationController
+  layout 'application_new'
+  
   def index
     if params[:recommend].present? && current_user.recommend_user_id.blank?
       current_user.update_attribute(:recommend_user_id, params[:recommend])
     end
-    
-    render text: "请使用微信客户端打开", layout: false and return if current_user.blank?
-    
-    render layout: 'application_new'
+    current_user
   end
   
   def about
-    render layout: 'application_new'
+    
   end
 end
