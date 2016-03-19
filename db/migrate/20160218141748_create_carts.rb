2002783fdaf66d407ee7d890216e9397ec2e60bd
backkom <99866770@qq.com>
2016-03-19 23:37:03 +0800

    修改购物车数量错误BUG

--- db/migrate/20160218141748_create_carts.rb
+++ db/migrate/20160218141748_create_carts.rb
@@ -3,7 +3,7 @@ class CreateCarts < ActiveRecord::Migration
     create_table :carts do |t|
       t.references  :user
       t.references  :product
-      t.integer     :total, defalt: 1
+      t.integer     :total, defalt: 0
       t.timestamps
     end
   end
