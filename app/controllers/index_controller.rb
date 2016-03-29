1bc1d1bcbc73803227acf0d973bf15d41153c754
backkom <99866770@qq.com>
2016-03-29 22:08:37 +0800

    禁止用户邀请自己

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,7 @@ class IndexController < ApplicationController
   
   def index
     if params[:recommend].present? && current_user.recommend_user_id.blank?
-      current_user.update_attribute(:recommend_user_id, params[:recommend])
+      current_user.update_attribute(:recommend_user_id, params[:recommend]) if params[:recommend].to_i != current_user.id
     end
     current_user
   end
